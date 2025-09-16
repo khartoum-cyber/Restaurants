4c66b69efb3ddb286ebe52d85a8c2a0a63aacc2d
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-09-16 20:30:03 +0200

    initial ef core setup

---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence
+{
+    internal class RestaurantsDbContext : DbContext
+    {
+        internal DbSet<Restaurant> Restaurants { get; set; }
+        internal DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-UQA779G\\SQLEXPRESS;Database=RestaurantsDb;Trusted_Connection=True;");
+        }
+        
+    }
+}
