67ba8fffb985201117dcd834ebfdb3aedddbb853
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-09-19 13:43:10 +0200

    DTOs for dish and restaurant

---
+++ Restaurants.Application/DTOs/RestaurantDTO.cs
@@ -0,0 +1,18 @@
+﻿using Restaurants.Application.Dishes.DTOs;
+
+namespace Restaurants.Application.DTOs
+{
+    internal class RestaurantDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Category { get; set; } = default!;
+        public bool HasDelivery { get; set; }
+        public string? City { get; set; }
+        public string? Street { get; set; }
+        public string? PostalCode { get; set; }
+
+        public List<DishDTO> Dishes { get; set; } = new();
+    }
+}
