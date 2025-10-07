532a09fd2ce6eb1df12be298346d56599fdf9afb
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-10-07 13:00:03 +0200

    custom user claims

---
+++ Restaurants.Infrastructure/Authorization/RestaurantsUserClaimsPrincipalFactory.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Authorization
+{
+    public class RestaurantsUserClaimsPrincipalFactory : UserClaimsPrincipalFactory<User, IdentityRole>
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IOptions<IdentityOptions> _options;
+
+        public RestaurantsUserClaimsPrincipalFactory(UserManager<Domain.Entities.User> userManager, RoleManager<IdentityRole> roleManager, IOptions<IdentityOptions> options) : base(userManager, roleManager, options)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _options = options;
+        }
+
+        public override async Task<ClaimsPrincipal> CreateAsync(User user)
+        {
+            var id = await GenerateClaimsAsync(user);
+
+            if (user.Nationality != null)
+            {
+                id.AddClaim(new Claim("Nationality", user.Nationality));
+            }
+
+            if (user.DateOfBirth != null)
+            {
+                id.AddClaim(new Claim("DateOfBirth", user.DateOfBirth.Value.ToString("yyyy-MM-dd")));
+            }
+
+            return new ClaimsPrincipal(id);
+        }
+    }
+}
