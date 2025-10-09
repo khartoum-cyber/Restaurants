ddc2b51950efa7fa8a72dbc1272f8f952b032c6b
Bartosz O. <bartosz.osmanski.external@eviden.com>
2025-10-09 10:41:45 +0200

    custom authorization

--- Restaurants.Infrastructure/Authorization/Requirements/MinimumAgeRequirementHandler.cs
+++ Restaurants.Infrastructure/Authorization/Requirements/MinimumAgeRequirementHandler.cs
@@ -1,12 +1,41 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using Restaurants.Application.User;
 
 namespace Restaurants.Infrastructure.Authorization.Requirements
 {
     public class MinimumAgeRequirementHandler : AuthorizationHandler<MinimumAgeRequirement>
     {
+        private readonly ILogger<MinimumAgeRequirementHandler> _logger;
+        private readonly IUserContext _userContext;
+
+        public MinimumAgeRequirementHandler(ILogger<MinimumAgeRequirementHandler> logger, IUserContext userContext)
+        {
+            _logger = logger;
+            _userContext = userContext;
+        }
         protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MinimumAgeRequirement requirement)
         {
-            throw new NotImplementedException();
+            var currentUser = _userContext.GetCurrentUser();
+
+            _logger.LogInformation("User: {Email}, date of birth {DoB} - Handling MinimumAgeRequirement", currentUser.Email, currentUser.DateOfBirth);
+
+            if (currentUser.DateOfBirth == null)
+            {
+                _logger.LogWarning("User date of birth is null");
+                context.Fail();
+                return Task.CompletedTask;
+            }
+
+            if (currentUser.DateOfBirth.Value.AddYears(requirement.MinimumAge) <= DateOnly.FromDateTime(DateTime.Today))
+            {
+                _logger.LogInformation("Authorization succeeded.");
+                context.Succeed(requirement);
+            }
+            else
+            {
+                context.Fail();
+            }
         }
     }
 }
--- Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Restaurants.Domain.Entities;
 using Restaurants.Domain.Repositories;
 using Restaurants.Infrastructure.Authorization;
+using Restaurants.Infrastructure.Authorization.Requirements;
 using Restaurants.Infrastructure.Persistence;
 using Restaurants.Infrastructure.Repositories;
 using Restaurants.Infrastructure.Seeders;
@@ -31,7 +33,9 @@ namespace Restaurants.Infrastructure.Extensions
             services.AddAuthorizationBuilder()
                 .AddPolicy(PolicyNames.HasNationality,
                     builder => builder.RequireClaim(AppClaimTypes.Nationality, "German", "Polish"))
-                .AddPolicy(PolicyNames.AtLeast20, builder => builder.AddRequirements());
+                .AddPolicy(PolicyNames.AtLeast20, builder => builder.AddRequirements(new MinimumAgeRequirement(20)));
+
+            services.AddScoped<IAuthorizationHandler, MinimumAgeRequirementHandler>();
         }
     }
 }
